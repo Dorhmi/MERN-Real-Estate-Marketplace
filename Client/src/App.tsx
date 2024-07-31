f279a2310df02611232f368610bbdf1bd3d7be40
Zakaria Dorhmi <dorhmizakaria@gmail.com>
2024-07-31 23:57:06 +0100

    create Pages & install Router-dom

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProfilePage from "./pages/ProfilePage";
+import SigninPage from "./pages/SigninPage";
+import SignupPage from "./pages/SignupPage";
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/signIn" element={<SigninPage />} />
+                <Route path="/signUp" element={<SignupPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
