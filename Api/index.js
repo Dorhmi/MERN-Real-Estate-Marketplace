ada62a7fc2e01933a9624f7bd4b744af6209837f
Zakaria Dorhmi <dorhmizakaria@gmail.com>
2024-08-04 00:30:52 +0100

    Add Swagger Docs

--- Api/index.js
+++ Api/index.js
@@ -2,17 +2,67 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import UserRouter from "./routes/UserRoute.js";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 
 const app = express();
 
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Real-Estate-Marketplace",
+            version: "1.0.0",
+        },
+    },
+    apis: ["./index.js"],
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 const URL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
 
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     responses:
+ *       200:
+ *         description: OK.
+ */
 app.get("/", (req, res) => {
     res.send("Welcom");
 });
+
+/**
+ * @swagger
+ * components:
+ *     schemas:
+ *        User:
+ *          type: object
+ *          properties:
+ *              username:
+ *                   type: string
+ *              email:
+ *                   type: string
+ *              password:
+ *                   type: string
+ */
+
+/**
+ * @swagger
+ *    /api/user/test:
+ *     get:
+ *      tags:
+ *        - User
+ *      responses:
+ *       200:
+ *         description: OK.
+ */
 app.use("/api/user", UserRouter);
 
 mongoose
